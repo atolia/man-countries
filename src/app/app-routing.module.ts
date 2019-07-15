7441a6db1872967ea0c240d671c472a7c774e215
rostyslav <rostik@telelan.com.ua>
2019-07-15 13:59:19 +0200

    added routing module components: CountriesList and PageNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CountriesListComponent } from './countries-list/countries-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '',
+    component: CountriesListComponent
+  },
+  { path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
